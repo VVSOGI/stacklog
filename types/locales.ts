065b2f200476cde27578c4669c5bf96e0ea5e349
benny <vvsogi@gmail.com>
2025-04-17 17:32:47 +0900

    Add new locales and cx utility function

--- types/locales.ts
+++ types/locales.ts
@@ -1,8 +1,14 @@
 export type Locales = 'en' | 'ko'
 
-export type Pages = 'home'
+export type Pages = 'home' | 'nav'
 
 export interface LocaleHome {
   job: string
   name: string
+}
+
+export interface LocaleNav {
+  home: string
+  project: string
+  blog: string
 }
