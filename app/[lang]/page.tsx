914c6798619791e8961a08bd05b0386fb7d161ed
benny <vvsogi@gmail.com>
2025-04-17 17:10:00 +0900

    Add page component and locale files for home page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,10 +1,19 @@
-export default function Page() {
+import { getLocales } from '@/utils/getLocales'
+import { LocaleHome, Locales } from '@/types'
+
+interface Props {
+  params: { lang: Locales }
+}
+
+export default async function Page({ params: { lang } }: Props) {
+  const locale = await getLocales<LocaleHome>('home', lang)
+
   return (
     <section>
       <div className="flex mb-8 font-semibold tracking-tighter">
         <div className="flex flex-col">
-          <h2 className="text-5xl mb-4">프론트엔드 개발자</h2>
-          <h1 className="text-6xl">김우석</h1>
+          <h2 className="text-5xl mb-4">{locale.job}</h2>
+          <h1 className="text-6xl">{locale.name}</h1>
         </div>
       </div>
       <p className="mb-4">
