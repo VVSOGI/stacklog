12bba6e16e759481cc47ddc3396327074af6f4af
benny <vvsogi@gmail.com>
2025-04-30 22:46:49 +0900

    Update font styles and creation dates in project page

--- app/[lang]/projects/page.tsx
+++ app/[lang]/projects/page.tsx
@@ -11,7 +11,7 @@ export default async function Page({ params: { lang } }: Props) {
 
   return (
     <div>
-      <h2 className="mb-4">{locale.title}</h2>
+      <h2 className="mb-4 font-normal">{locale.title}</h2>
       <div
         className={`
           grid grid-cols-1 gap-4
@@ -23,10 +23,13 @@ export default async function Page({ params: { lang } }: Props) {
             <a key={project.title} href={`/${lang}/projects/${project.page}`} className="h-fit flex flex-col gap-2 pb-8 cursor-pointer">
               <img className="object-contain w-full h-[300px]" src={project.banner} alt={project.banner} />
               <div className="flex flex-col">
-                <h5 className="font-bold">{project.title}</h5>
+                <h5 className="font-medium">{project.title}</h5>
                 <p className="text-gray-600">{project.motivate}</p>
               </div>
-              <p className="text-sm text-gray-500">{formatDate(project.createdAt)}</p>
+              <div className="flex items-center gap-2">
+                <p className="text-sm text-green-800">POST</p>
+                <p className="text-sm text-gray-500">{formatDate(project.createdAt)}</p>
+              </div>
             </a>
           )
         })}
