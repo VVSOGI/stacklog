b34201a3e341c76e4e8a7199ce3d7c025bfb7567
benny <vvsogi@gmail.com>
2025-04-30 17:54:43 +0900

    Refactor page component and add locale title

--- app/[lang]/projects/page.tsx
+++ app/[lang]/projects/page.tsx
@@ -11,6 +11,7 @@ export default async function Page({ params: { lang } }: Props) {
 
   return (
     <div>
+      <h2 className="mb-4">{locale.title}</h2>
       <div
         className={`
           grid grid-cols-1 gap-4
@@ -20,7 +21,7 @@ export default async function Page({ params: { lang } }: Props) {
         {locale.list.map((project) => {
           return (
             <a key={project.title} href={`/${lang}/projects/${project.page}`} className="h-fit flex flex-col gap-2 pb-8 cursor-pointer">
-              <img className="object-contain w-full h-[300px]" src={project.images.list[0].url} alt={project.images.list[0].alt} />
+              <img className="object-contain w-full h-[300px]" src={project.banner} alt={project.banner} />
               <div className="flex flex-col">
                 <h5 className="font-bold">{project.title}</h5>
                 <p className="text-gray-600">{project.motivate}</p>
